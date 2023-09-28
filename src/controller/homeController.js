6febc138b9f2283b8667b73e825e61d35871b406
LeKiem <levankiemdev@gmail.com>
2023-09-28 20:28:36 +0700

    #21 Kết Nối Tới MySQL Database với Sequelize - Render Dữ Liệu Ra View

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,15 @@
-let getHomePage = (req, res) => {
-  return res.render("homepage.ejs");
+import db from "../models/index";
+
+let getHomePage = async (req, res) => {
+  let data = await db.User.findAll();
+  console.log(data);
+  try {
+    return res.render("homepage.ejs", {
+      data: JSON.stringify(data)
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 let getAboutPage = (req, res) => {
