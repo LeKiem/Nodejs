f029025332c429d997dd10529fc68f99a92f1a26
LeKiem <levankiemdev@gmail.com>
2023-09-30 11:36:38 +0700

    #27 Sửa Dữ liệu (Edit) - Phần 3

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -8,7 +8,7 @@ let createNewUser = async data => {
     try {
       let hashPasswordFromBcrypt = await hasUserPassword(data.password);
       await db.User.create({
-        email: data.STRING,
+        email: data.email,
         password: hashPasswordFromBcrypt,
         firstName: data.firstName,
         lastName: data.lastName,
@@ -48,8 +48,51 @@ let getAllUser = () => {
     }
   });
 };
+let getUserInfoById = userId => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: userId },
+        raw: true
+      });
+
+      if (user) {
+        resolve(user);
+      } else {
+        resolve({});
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let updateUserData = data => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let user = await db.User.findOne({
+        where: { id: data.id }
+      });
+
+      if (user) {
+        user.firstName = data.firstName;
+        user.lastName = data.lastName;
+        user.address = data.address;
+        await user.save();
+        let allUsers = await db.User.findAll();
+        resolve(allUsers);
+      } else {
+        resolve();
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
+};
 
 module.exports = {
   createNewUser: createNewUser,
-  getAllUser: getAllUser
+  getAllUser: getAllUser,
+  getUserInfoById: getUserInfoById,
+  updateUserData: updateUserData
 };
