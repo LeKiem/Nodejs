374d73e8d36e4522cc10dfee5b7eb2fca009ff36
LeKiem <levankiemdev@gmail.com>
2023-10-02 10:57:54 +0700

    #41 CRUD React 5 - Thêm Mới Người Dùng (CREATE)

--- src/services/userService.js
+++ src/services/userService.js
@@ -108,24 +108,25 @@ let createNewUser = data => {
       if (check === true) {
         resolve({
           errCode: 1,
-          message: "Your email is already in used , Plz try another"
+          errMessage: "Your email is already in used , Plz try another"
+        });
+      } else {
+        let hashPasswordFromBcrypt = await hasUserPassword(data.password);
+        await db.User.create({
+          email: data.email,
+          password: hashPasswordFromBcrypt,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          address: data.address,
+          phonenumber: data.phonenumber,
+          gender: data.gender === "1" ? true : false,
+          roleId: data.roleId
+        });
+        resolve({
+          errCode: 0,
+          message: "OK"
         });
       }
-      let hashPasswordFromBcrypt = await hasUserPassword(data.password);
-      await db.User.create({
-        email: data.email,
-        password: hashPasswordFromBcrypt,
-        firstName: data.firstName,
-        lastName: data.lastName,
-        address: data.address,
-        phonenumber: data.phonenumber,
-        gender: data.gender === "1" ? true : false,
-        roleId: data.roleId
-      });
-      resolve({
-        errCode: 0,
-        message: "OK"
-      });
     } catch (e) {
       reject(e);
     }
