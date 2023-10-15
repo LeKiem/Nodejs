d514c5e6014b15509c069d2f9635186f15f9b79a
LeKiem <levankiemdev@gmail.com>
2023-10-15 20:17:36 +0700

    #88 Xử Lý API Xác Nhận Lịch Hẹn Khám Bệnh Qua Email

--- src/services/patientServices.js
+++ src/services/patientServices.js
@@ -1,6 +1,12 @@
 import db from "../models/index";
 require("dotenv").config();
 import emailService from "./emailService";
+import { v4 as uuidv4 } from "uuid";
+
+let buiUrlEmail = (doctorId, token) => {
+  let result = `${process.env.URL_REACT}/verify-booking?token=${token}&doctorId=${doctorId}`;
+  return result;
+};
 
 let postBookAppointment = (data) => {
   return new Promise(async (resolve, reject) => {
@@ -19,15 +25,14 @@ let postBookAppointment = (data) => {
           errMessage: "Missing  parameters"
         });
       } else {
-        // let token = uuidv4();
+        let token = uuidv4();
         await emailService.sendSimpleEmail({
           reciverEmail: data.email,
           patientName: data.fullName,
           time: data.timeString,
           doctorName: data.doctorName,
           language: data.language,
-          redirectLink: "https://www.youtube.com/"
-          // redirectLink: buiUrlEmail(data.doctorId, token)
+          redirectLink: buiUrlEmail(data.doctorId, token)
         });
         // console.log(doctorName);
 
@@ -51,8 +56,8 @@ let postBookAppointment = (data) => {
               doctorId: data.doctorId,
               patientId: user[0].id,
               date: data.date,
-              timeType: data.timeType
-              // token: token
+              timeType: data.timeType,
+              token: token
             }
           });
         }
@@ -68,6 +73,45 @@ let postBookAppointment = (data) => {
   });
 };
 
+let postVerifyBookAppointment = (data) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!data.token || !data.doctorId) {
+        resolve({
+          errCode: 1,
+          errMessage: "Missing  parameters"
+        });
+      } else {
+        let appointment = await db.Booking.findOne({
+          where: {
+            doctorId: data.doctorId,
+            token: data.token,
+            statusId: "S1"
+          },
+          raw: false
+        });
+
+        if (appointment) {
+          appointment.statusId = "S2";
+          await appointment.save();
+          resolve({
+            errCode: 0,
+            errMessage: "Update the appointment succeed!"
+          });
+        } else {
+          resolve({
+            errCode: 2,
+            errMessage: "Appointment has been scheduled or does not exist"
+          });
+        }
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
 module.exports = {
-  postBookAppointment: postBookAppointment
+  postBookAppointment: postBookAppointment,
+  postVerifyBookAppointment: postVerifyBookAppointment
 };
