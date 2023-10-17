05c3c628c6dadf38a8c7010897af0f4fbb24a6d3
LeKiem <levankiemdev@gmail.com>
2023-10-17 13:30:24 +0700

    #96 Tạo Dữ Liệu Các Cơ Sở Y Tế/Phòng Khám Bệnh

--- src/route/web.js
+++ src/route/web.js
@@ -5,6 +5,7 @@ import userController from "../controller/userController";
 import doctorController from "../controller/doctorController";
 import patientController from "../controller/patientController";
 import specialtyController from "../controller/specialtyController";
+import clinicController from "../controller/clinicController";
 
 let router = express.Router();
 
@@ -62,6 +63,12 @@ let initWebRoutes = (app) => {
     specialtyController.getDetailSpecialtyById
   );
 
+  router.post("/api/create-new-clinic", clinicController.createClinic);
+  // router.get("/api/get-clinic", clinicController.getAllClinic);
+  // router.get(
+  //   "/api/get-detail-clinic-by-id",
+  //   clinicController.getDetailClinicById
+  // );
   return app.use("/", router);
 };
 module.exports = initWebRoutes;
