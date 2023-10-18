afcb1aca041861ca6b1ac09f38f4416fcef7c3f1
LeKiem <levankiemdev@gmail.com>
2023-10-18 09:41:20 +0700

    #97 Hoàn Thiện Chức Năng Xem Chi Tiết Phòng Khám và Cơ Sở Y Tế

--- src/route/web.js
+++ src/route/web.js
@@ -64,11 +64,11 @@ let initWebRoutes = (app) => {
   );
 
   router.post("/api/create-new-clinic", clinicController.createClinic);
-  // router.get("/api/get-clinic", clinicController.getAllClinic);
-  // router.get(
-  //   "/api/get-detail-clinic-by-id",
-  //   clinicController.getDetailClinicById
-  // );
+  router.get("/api/get-clinic", clinicController.getAllClinic);
+  router.get(
+    "/api/get-detail-clinic-by-id",
+    clinicController.getDetailClinicById
+  );
   return app.use("/", router);
 };
 module.exports = initWebRoutes;
